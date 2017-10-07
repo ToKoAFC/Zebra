b1316b577cac84f7318580be7739b16f07a81130
ToKoAFC <tomaszkrupa95@gmail.com>
2017-10-07 19:33:06 +0200

    add Products Controller and View

---
+++ Presentation/Zebra.Web/Controllers/ProductsController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Globalization;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.Owin.Security;
+using Zebra.Web.Models;
+using Zebra.Database.Models;
+
+namespace Zebra.Web.Controllers
+{
+    [Authorize]
+    public class ProductsController : Controller
+    {
+        public ProductsController()
+        {
+        }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
