8db432c31c33204a955f14fb067e5ce6bffa90fd
ToKoAFC <tomaszkrupa95@gmail.com>
2017-10-07 15:02:22 +0200

    change Context for IdentityDbContext

--- AccessLayer/Zebra.Database/ZebraContext.cs
+++ AccessLayer/Zebra.Database/ZebraContext.cs
@@ -1,8 +1,24 @@
-﻿using System.Data.Entity;
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using System.Data.Entity;
+using Zebra.Database.Models;
 
 namespace Zebra.Database
 {
-    public class ZebraContext : DbContext
+    public class ZebraContext : IdentityDbContext<DbAppUser>
     {
+        public ZebraContext()
+            : base("ZebraContext", throwIfV1Schema: false)
+        {
+        }
+        protected override void OnModelCreating(DbModelBuilder builder)
+        {
+            System.Data.Entity.Database.SetInitializer<ZebraContext>(null);
+            base.OnModelCreating(builder);
+        }
+
+        public static ZebraContext Create()
+        {
+            return new ZebraContext();
+        }
     }
 }
