6e2f268554f14a5e04f4c836cc346bfe17f90f6f
ToKoAFC <tomaszkrupa95@gmail.com>
2017-10-07 13:57:17 +0200

    Add architecture sketch

---
+++ LogicLayer/Zebra.CoreModels/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zebra.CoreModels
+{
+    public class Class1
+    {
+    }
+}
---
+++ Presentation/Zebra.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Zebra.Web.Startup))]
+namespace Zebra.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
