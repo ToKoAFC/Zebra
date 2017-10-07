3b996faa05b45a8cfb5cbf409255d1184725dd0e
ToKoAFC <tomaszkrupa95@gmail.com>
2017-10-07 20:29:19 +0200

    Add DbCompany model

--- AccessLayer/Zebra.Database.Models/Models/DbAppUser.cs
+++ AccessLayer/Zebra.Database.Models/Models/DbAppUser.cs
@@ -1,5 +1,8 @@
 ﻿using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Security.Claims;
 using System.Threading.Tasks;
 
@@ -7,6 +10,23 @@ namespace Zebra.Database.Models
 {
     public class DbAppUser : IdentityUser
     {
+        public DbAppUser()
+        {
+            Id = new Guid().ToString();
+        }
+
+        [StringLength(50)]
+        public string FirstName { get; set; }
+
+        [StringLength(256)]
+        public string SecondName { get; set; }
+
+        public int? CompanyId { get; set; }
+
+        [ForeignKey("CompanyId")]
+        public virtual DbCompany Company { get; set; }
+
+
         public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<DbAppUser> manager)
         {
             // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
---
+++ AccessLayer/Zebra.Database.Models/Models/DbCompany.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace Zebra.Database.Models
+{
+    public class DbCompany
+    {
+        public DbCompany()
+        {
+            Users = new HashSet<DbAppUser>();
+        }
+
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Name { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string City { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Adress { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        [DataType(DataType.PostalCode)]
+        public string PostalCode { get; set; }
+
+        public string Description { get; set; }
+
+        public bool IsDeleted { get; set; }
+
+        public virtual ICollection<DbAppUser> Users { get; set; }
+    }
+}
