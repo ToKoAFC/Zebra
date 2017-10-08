0b9536e0d40439003c7dce1227abd4be0f62ebd4
ToKoAFC <tomaszkrupa95@gmail.com>
2017-10-08 14:11:59 +0200

    Add AdminProductsController and view

---
+++ Presentation/Zebra.Web/Controllers/AdminProductsController.cs
@@ -0,0 +1,16 @@
+﻿using System.Web.Mvc;
+
+namespace Zebra.Web.Controllers
+{
+    public class AdminProductsController : Controller
+    {
+        public AdminProductsController()
+        {
+        }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
