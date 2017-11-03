45ca1afd3957cf62d1555b546896eba0ab6d3c4b
ToKoAFC <tomaszkrupa95@gmail.com>
2017-11-03 22:25:56 +0100

    add AdminPriceController

---
+++ Presentation/Zebra.Web/Controllers/AdminPriceController.cs
@@ -0,0 +1,16 @@
+﻿using System.Web.Mvc;
+
+namespace Zebra.Web.Controllers
+{
+    public class AdminPriceController : Controller
+    {
+        public AdminPriceController()
+        {
+        }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
